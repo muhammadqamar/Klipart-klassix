cd3240e93dc378f3e81dec23e17c360b925e9f92
muhammadqamar <muhammadqamar111@gmail.com>
2022-01-25 00:45:04 +0500

    added a gif

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,35 +2,28 @@ import React, { useState, useEffect } from "react"
 
 import Faq from "./Faq"
 import KlipVideo from "../../assets/video/Klipart-movie.mp4"
-// import img1 from "../../assets/image/image-1.jpg"
-// import img2 from "../../assets/image/image-2.jpg"
-// import img3 from "../../assets/image/image-3.jpg"
-// import img4 from "../../assets/image/image-4.jpg"
+import gif from "../../assets/image/main.gif"
 
-const Image = [
-  { img1: "../../assets/image/image-1.jpg" },
-  { img1: "../../assets/image/image-2.jpg" },
-  { img1: "../../assets/image/image-3.jpg" },
-  { img1: "../../assets/image/image-4.jpg" },
-]
+
 
 const Index = () => {
-  const [runImg, setRunImg] = useState(Image)
-  console.log(runImg)
+
 
   return (
     <div className="container">
-      <h1>home</h1>
-      <div style={{ width: "300px", height: "400px", backgroundColor: "#fff" }}>
-        {/* <img src={runImg} alt="xcdd" /> */}
+
+      <div className="img-gif" >
+        <img src={gif} alt="img"   />
       </div>
       <div>
         <video
           src={KlipVideo}
           width="100%"
           height="600"
-          controls="controls"
-          autoplay="true"
+
+
+          muted
+          autoPlay
         />
       </div>
       <Faq />
